dd2509d9673d36e2e8d970c5922dd9849fd5c222
Ali Albaali <ali@albaali.com>
2021-01-08 09:54:43 +0300

    Add Navigation Component

--- app/src/main/java/com/confy/app/MainActivity.java
+++ app/src/main/java/com/confy/app/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.confy.R;
+import com.confy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
     }
 }
