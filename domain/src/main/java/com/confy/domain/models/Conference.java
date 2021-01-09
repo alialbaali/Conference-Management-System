678da88006b549482b668ffa1d057547dd27161a
Ali Albaali <ali@albaali.com>
2021-01-09 19:30:49 +0300

    Add start and end date attributes to Conference

--- domain/src/main/java/com/confy/domain/models/Conference.java
+++ domain/src/main/java/com/confy/domain/models/Conference.java
@@ -1,13 +1,22 @@
 package com.confy.domain.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.time.LocalDateTime;
+
+@Entity(tableName = "conferences")
 public class Conference {
 
     @PrimaryKey
     String id;
     String name;
     String description;
+    @ColumnInfo(name = "start_date")
+    LocalDateTime startDate;
+    @ColumnInfo(name = "end_date")
+    LocalDateTime endDate;
 
     public Conference(String name, String description) {
         this.name = name;
@@ -15,9 +24,13 @@ public class Conference {
     }
 
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public String getDescription() { return description; }
+    public String getDescription() {
+        return description;
+    }
 
     // Add the other fields...
 }
