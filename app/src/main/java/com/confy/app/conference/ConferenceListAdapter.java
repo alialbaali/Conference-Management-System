53bb7e9a293d612ab4cce096ff360a3558da9c6e
Bekir Malik Özkuran <bekir.ozkuran@isik.edu.tr>
2021-01-09 19:12:33 +0300

    Create ConferenceListAdapter.java

---
+++ app/src/main/java/com/confy/app/conference/ConferenceListAdapter.java
@@ -0,0 +1,22 @@
+package com.confy.app.conference;
+
+import android.widget.*;
+import android.support.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.confy.domain.models.Conference;
+
+public class ConferenceListAdapter {
+    Conference[] conferenceList;
+
+    public ConferenceListAdapter(Conference[] conferenceList) {
+        this.conferenceList = conferenceList;
+    }
+
+    //TODO
+}
