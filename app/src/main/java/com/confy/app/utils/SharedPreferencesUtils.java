f5d5445503005acc7f58d95f35123f20446d42b7
Ali Albaali <ali@albaali.com>
2021-01-10 20:58:03 +0300

    Add SharedPreferencesUtils

---
+++ app/src/main/java/com/confy/app/utils/SharedPreferencesUtils.java
@@ -0,0 +1,9 @@
+package com.confy.app.utils;
+
+public final class SharedPreferencesUtils {
+
+    public static String SHARED_PREFERENCES_NAME = "Confy Shared Preferences";
+
+    public static String IS_USER_SIGNED_IN = "is_user_signed_in";
+
+}
